faf552fa361f608996f91bfd19d5cacedca54037
Keo  Kay <keokay888@gmail.com>
2024-03-29 14:48:01 +0700

    done by autopush.sh

--- src/main/java/co/istad/mobilebankingcstad/features/files/FileService.java
+++ src/main/java/co/istad/mobilebankingcstad/features/files/FileService.java
@@ -2,6 +2,8 @@ package co.istad.mobilebankingcstad.features.files;
 
 import co.istad.mobilebankingcstad.features.files.dto.FileResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -9,5 +11,8 @@ import java.util.List;
 public interface FileService {
     FileResponse uploadSingleFile(MultipartFile file, HttpServletRequest request);
     List<String> uploadMultipleFiles(MultipartFile[] files);
+
+
+    ResponseEntity<Resource> serveFile(String filename, HttpServletRequest request);
     void deleteFile(String filename);
 }
--- src/main/java/co/istad/mobilebankingcstad/features/files/FileServiceImpl.java
+++ src/main/java/co/istad/mobilebankingcstad/features/files/FileServiceImpl.java
@@ -3,10 +3,16 @@ package co.istad.mobilebankingcstad.features.files;
 import co.istad.mobilebankingcstad.features.files.dto.FileResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -21,15 +27,23 @@ public class FileServiceImpl implements FileService {
     @Value("${file.storage-dir}")
     String fileStorageDir;
 
-
-    private String generateImageUrl(HttpServletRequest request,String filename){
+    //http://localhost:8888/api/v1/files/download/3f54df29-31f0-4c74-9552-62dfca9e4f1e.png
+    private String generateImageUrl(HttpServletRequest request, String filename) {
         return String.format("%s://%s:%d/images/%s",
                 request.getScheme(),
                 request.getServerName(),
                 request.getServerPort(),
                 filename);
     }
-    private String uploadFile(MultipartFile file)  {
+    private String generateDownloadImageUrl(HttpServletRequest request, String filename) {
+        return String.format("%s://%s:%d/api/v1/files/download/%s",
+                request.getScheme(),
+                request.getServerName(),
+                request.getServerPort(),
+                filename);
+    }
+
+    private String uploadFile(MultipartFile file) {
         try {
 //       Check if the directory doesn't exist , we will create the directory
             Path fileStoragePath = Path.of(fileStorageDir);
@@ -45,7 +59,7 @@ public class FileServiceImpl implements FileService {
                     fileStoragePath.resolve(fileName),
                     StandardCopyOption.REPLACE_EXISTING);
             return fileName;
-        } catch (IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
             return null;
         }
@@ -56,12 +70,12 @@ public class FileServiceImpl implements FileService {
     public FileResponse uploadSingleFile(MultipartFile file, HttpServletRequest request) {
 
         String filename = uploadFile(file);
-        String fullImageUrl = generateImageUrl(request,filename);
+        String fullImageUrl = generateImageUrl(request, filename);
 
         return FileResponse.builder()
-                .downloadUrl("null")
+                .downloadUrl(generateDownloadImageUrl(request,filename))
                 .fileType(file.getContentType())
-                .size(file.getSize())
+                .size((float) file.getSize() / 1024) // in KB
                 .filename(filename)
                 .fullUrl(fullImageUrl).build();
     }
@@ -73,6 +87,29 @@ public class FileServiceImpl implements FileService {
             fileNames.add(uploadFile(file));
         }
         return fileNames;
+    }
+
+    @Override
+    public ResponseEntity<Resource> serveFile(String filename, HttpServletRequest request) {
+        try {
+            //        get path of the images
+            Path imagePath = Path.of(fileStorageDir).resolve(filename);
+            Resource resourceUrl = new UrlResource(imagePath.toUri());
+
+            if(resourceUrl.exists()){
+                return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType("image/jpeg"))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resourceUrl.getFilename() + "\"")
+                    .body(resourceUrl);
+            }else {
+                // bad request
+                throw new RuntimeException("Resources not found ! ");
+            }
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
     }
 
     @Override
